02866f839c163e335c89a687ef7e939f677efb01
Xiang41023113 <41023113@gm.nfu.edu.tw>
2022-12-11 12:40:00 +0000

    W6

---
+++ downloads/W6.js
@@ -0,0 +1,15 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 930, y: 528, width: 60, height: 24 }, target: 3 },
+    ],
+    "89": [
+      { type: "gotoframe", rect: { x: 930, y: 528, width: 60, height: 24 }, target: 2 },
+      { type: "gotoframe", rect: { x: 930, y: 528, width: 60, height: 24 }, target: 90 },
+    ],
+  },
+};
