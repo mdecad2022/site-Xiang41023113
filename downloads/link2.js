47655d0587b7157b280705b4c0b6159be5891609
Xiang41023113 <41023113@gm.nfu.edu.tw>
2022-12-19 02:33:51 +0000

    影片更正

--- downloads/link2.js
+++ downloads/link2.js
@@ -4,5 +4,8 @@
   buttonFrameLength: 5,
   buttonFrameOffset: 2,
   frameStops: {
+    "627": [
+      { type: "gotoframe", rect: { x: 1402, y: 128, width: 60, height: 24 }, target: 628 },
+    ],
   },
 };
