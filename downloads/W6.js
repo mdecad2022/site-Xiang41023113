abdc2b90262a98e12ec681615c8f62de72c293a4
Xiang41023113 <41023113@gm.nfu.edu.tw>
2023-01-05 12:12:29 +0000

    w64

--- downloads/W6.js
+++ downloads/W6.js
@@ -4,12 +4,5 @@
   buttonFrameLength: 5,
   buttonFrameOffset: 2,
   frameStops: {
-    "2": [
-      { type: "gotoframe", rect: { x: 930, y: 528, width: 60, height: 24 }, target: 3 },
-    ],
-    "89": [
-      { type: "gotoframe", rect: { x: 930, y: 528, width: 60, height: 24 }, target: 2 },
-      { type: "gotoframe", rect: { x: 930, y: 528, width: 60, height: 24 }, target: 90 },
-    ],
   },
 };
